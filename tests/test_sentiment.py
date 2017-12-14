5d3b9cc5db02b45a3d15192bb4f71db32f3eba55
A. Besir Kurtulmus <besir@algorithmia.com>
2017-12-14 13:55:46 -0800

    Add some tests for Apple Computer Twitter Sentiment

--- tests/test_sentiment.py
+++ tests/test_sentiment.py
@@ -4,20 +4,36 @@
 # Comparing sentiment analysis algorithms unit-test
 # Dataset from: http://ai.stanford.edu/~amaas/data/sentiment/
 
-def get_dataset():
+from dataset import AppleComputersTwitterSentiment
+
+ds = AppleComputersTwitterSentiment()
+
+def test_dataset_downloaded():
+    assert not isinstance(ds, type(None))
+
+def test_verify_positive_sentiment_values():
+    # There should only be 423 positive tweets
+    assert len(ds.get_positive_sentiment()) == 423
+
+def test_verify_neutral_sentiment_values():
+    # There should only be 2162 neutral tweets
+    assert len(ds.get_neutral_sentiment()) == 2162
+
+def test_verify_negative_sentiment_values():
+    # There should only be 1219 negative tweets
+    assert len(ds.get_negative_sentiment()) == 1219
+
+def test_load_dataset():
     assert(False)
 
-def load_dataset():
+def test_define_algorithms():
     assert(False)
 
-def define_algorithms():
+def test_create_glues():
     assert(False)
 
-def create_glues():
+def test_create_metrics():
     assert(False)
 
-def create_metrics():
-    assert(False)
-
-def create_competition():
+def test_create_competition():
     assert(False)
