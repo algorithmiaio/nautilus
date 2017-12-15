55cf93abdd2a34ef06370a12e9a50dde7d552515
A. Besir Kurtulmus <besir@algorithmia.com>
2017-12-14 16:56:19 -0800

    Change glue structure and add new glues

--- glue.py
+++ glue.py
@@ -4,8 +4,8 @@ from algo_type import AlgoTypes
 
 class Glue(object):
     '''
-    Glue is an adaptor for algorithm I/O's. It creates a common standard for
-        calling algorithms.
+    Glue is an I/O adaptor for algorithms. It creates a common standard for
+        interacting with algorithms in metrics.
 
     Currently 3 types of glues are supported: classification, regression and
         multilabel.
@@ -18,10 +18,50 @@ class Glue(object):
         if not self.types.valid_type(self.type):
             raise Exception("Algorithm is not a valid type.")
 
-    def input(self, algo_input):
+    def input(self, user_input):
+        # This takes the user input, and casts it into the algo input.
         # override this method
+        algo_input = user_input
         return algo_input
 
     def output(self, algo_output):
+        # This takes the algo output, and casts it into the user output.
         # override this method
-        return algo_output
+        user_output = algo_output
+        return user_output
+
+class SentimentAnalysisGlue(Glue):
+    '''
+    Glue object for sentiment analysis.
+
+    Returns sentiment object with positive, neutral, negative and compound
+        float values.
+    '''
+    def __init__(self):
+        super().__init__("classification")
+        self.sentiment = {}
+        self.sentiment["positive"] = None
+        self.sentiment["neutral"] = None
+        self.sentiment["negative"] = None
+        self.sentiment["compound"] = None
+
+class Algorithmia_Nlp_SentimentAnalysis(SentimentAnalysisGlue):
+    def __init__(self):
+        super().__init__()
+        self.input_structure = {}
+        self.output_structure = {}
+        self.input_structure["document"] = str
+        self.output_structure["sentiment"] = float
+        self.output_structure["document"] = str
+
+    def input(self, user_input):
+        algo_input = {}
+        if not isinstance(i, str):
+            raise Exception("Input must be a string.")
+        algo_input["document"] = i
+        return algo_input
+
+    def output(self, algo_output):
+        user_output = {}
+        self.sentiment["compound"] = algo_output[0]["sentiment"]
+        return self.sentiment
