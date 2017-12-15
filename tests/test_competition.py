385baa3467ff98104f7af2339673f99a880736f2
A. Besir Kurtulmus <besir@algorithmia.com>
2017-12-14 16:56:01 -0800

    Create empty test files

---
+++ tests/test_competition.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Author: A. Besir Kurtulmus
+
+# Comparing sentiment analysis algorithms unit-test
+# Dataset from: http://ai.stanford.edu/~amaas/data/sentiment/
+
+# To run this test, run the following in the project root directory:
+# python -m pytest tests/test_sentiment_competition.py
---
+++ tests/test_sentiment_glue.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Author: A. Besir Kurtulmus
+
+# Comparing sentiment analysis algorithms unit-test
+# Dataset from: http://ai.stanford.edu/~amaas/data/sentiment/
+
+# To run this test, run the following in the project root directory:
+# python -m pytest tests/test_sentiment_glue.py
+from glue import Algorithmia_Nlp_SentimentAnalysis
